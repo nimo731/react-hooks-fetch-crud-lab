0852c8ccfce62b78b16b7b7b6c4f2b0b5d230be6
nimo731 <patience.karanja@student.moringaschool.com>
2025-04-23 09:25:15 +0300

    Add mock data for testing

---
+++ src/mocks/questions.js
@@ -0,0 +1,14 @@
+export const mockQuestions = [
+  {
+    id: 1,
+    prompt: "What is your favorite color?",
+    answers: ["Red", "Blue", "Green", "Yellow"],
+    correctIndex: 1
+  },
+  {
+    id: 2,
+    prompt: "What is your favorite animal?",
+    answers: ["Dog", "Cat", "Bird", "Fish"],
+    correctIndex: 0
+  }
+]; 
